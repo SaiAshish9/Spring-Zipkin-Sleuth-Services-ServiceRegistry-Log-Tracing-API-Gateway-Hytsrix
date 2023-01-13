42ef59cd9e4bcb736363e4310d50195e504d6ef5
SaiAshish <saiashish3760@gmail.com>
2023-01-14 02:19:02 +0530

    Add files via upload

---
+++ user-service/src/main/java/com/spring/userservice/VO/ResponseTemplateVO.java
@@ -0,0 +1,15 @@
+package com.spring.userservice.VO;
+
+import com.spring.userservice.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+
+    private User user;
+    private Department department;
+}
---
+++ user-service/src/main/java/com/spring/userservice/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.spring.userservice.controller;
+
+import com.spring.userservice.VO.ResponseTemplateVO;
+import com.spring.userservice.entity.User;
+import com.spring.userservice.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/")
+    public User saveUser(@RequestBody User user) {
+        log.info("Inside saveUser of UserController");
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseTemplateVO getUserWithDepartment(@PathVariable("id") Long userId) {
+        log.info("Inside getUserWithDepartment of UserController");
+        return userService.getUserWithDepartment(userId);
+    }
+
+
+}
